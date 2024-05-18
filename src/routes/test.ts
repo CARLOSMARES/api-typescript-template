ff69c9d88cc33539fb8cd3a5f8d22385520550a0
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-05-17 22:14:53 -0600

    Nueva Version de la api mejorada

--- src/routes/test.ts
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-import testFunction from '../controllers/test.controller';
-
-const router = Express.Router();
-
-router.get("/testRoute", testFunction);
-
-export default router;
