5e779bad9bd9c4f26a06acff085bbf98efa27439
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2024-09-20 13:55:13 -0600

    se agrega body-parser, se modifica el dependabot

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,43 @@
+import bodyParser from "body-parser";
 import express from "express";
 import http, { Server } from "http";
 import { API } from "./interfaces";
 import * as routes from "./routes/routes";
 
 export class ExpressApi implements API {
+
   private router: express.Router;
+
   private baseUrl: string;
 
   constructor(baseUrl: string) {
+
     this.baseUrl = baseUrl;
+
     this.router = express.Router();
 
     this.router.get("/", routes.welcome);
+
     this.router.get("/health", routes.healthCheck);
+
   }
 
   public createServer = (): Server => {
+
     const expressApp: express.Application = express();
+
+    expressApp.use(bodyParser.json());
+
+    expressApp.use(bodyParser.urlencoded(
+      {
+        extended: true
+      }
+    ));
 
     expressApp.use("/", this.router);
 
     return http.createServer(expressApp);
+
   };
+
 }
